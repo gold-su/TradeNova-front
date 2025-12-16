8a7f2572bbf740b9504b057d44de384ee90142ec
pds02 <pds020130@naver.com>
2025-12-17 02:27:03 +0900

    first commit

---
+++ tradenova-frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+    return (
+        <div style={{ padding: 24, fontSize: 32, color: "white" }}>
+            Login
+        </div>
+    );
+};
+
+export default LoginPage;
---
+++ tradenova-frontend/src/pages/SignupPage.tsx
@@ -0,0 +1,9 @@
+const SignupPage = () => {
+    return (
+        <div style={{ padding: 24, fontSize: 32, color: "white" }}>
+            Signup
+        </div>
+    );
+};
+
+export default SignupPage;
